c5b2c3e23f614d61adf7d462f9b04209dfdad3ab
Jenna Pimenta <21pimenta@stfrancishs.org>
2018-08-31 20:01:51 +0000

    init-21pimenta

---
+++ p27chicken.c
@@ -0,0 +1,12 @@
+// whitespace
+#include <stdio.h>
+main()
+{
+float a, b;
+printf("How much of a bonus did you get? ");
+scanf(" %f", &a);
+b = .85*a;
+printf("If you give 15 percent to charity, you will still ");
+printf("have %.2f.", b);
+return 0;
+}
